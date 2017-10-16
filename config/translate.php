38597b211173a296fac4e4a2246c5d9799a0cd5b
lastgleam <lastgleam@gmail.com>
2017-10-16 18:49:34 +0900

    add config for translate

---
+++ config/translate.php
@@ -0,0 +1,27 @@
+<?php
+ return [
+  country_jp => [
+      'japan'        => '日本',
+      'korea'        => '韓国',
+      'india'        => 'インド',
+      'vietnam'      => 'ベトナム',
+      'china'        => '中国',
+      'usa'          => '米国'
+    ],
+  gender_jp => [
+      'male'         => '男',
+      'female'       => '女'
+  ],
+  hobby_jp => [
+      'sports'       => 'スポーツ',
+      'music'        => '音楽',
+      'cinema'       => '映画',
+      'shopping'     => '買い物',
+      'manga/anime'  => '漫画/アニメ',
+      'book'         => '読書',
+      'prowrestling' => 'プロレス',
+      'delusion'     => '妄想',
+      'game'         => 'ゲーム'
+  ]
+ ];
+?>
--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -1,29 +1,9 @@
 <!doctype html>
 @php
    //引き継がれた値を日本語に変換
-        $country_jp = [
-            'japan'        => '日本',
-            'korea'        => '韓国',
-            'india'        => 'インド',
-            'vietnam'      => 'ベトナム',
-            'china'        => '中国',
-            'usa'          => '米国'
-        ];
-        $gender_jp = [
-            'male'         => '男',
-            'female'       => '女'
-        ];
-        $hobby_jp = [
-            'sports'       => 'スポーツ',
-            'music'        => '音楽',
-            'cinema'       => '映画',
-            'shopping'     => '買い物',
-            'manga/anime'  => '漫画/アニメ',
-            'book'         => '読書',
-            'prowrestling' => 'プロレス',
-            'delusion'     => '妄想',
-            'game'         => 'ゲーム'
-        ];
+        $country_jp = config('translate.country_jp');
+        $gender_jp = config('translate.gender_jp');
+        $hobby_jp = config('translate.hobby_jp');
 
         //国籍を変換
         $result['country'] = $country_jp[$result['country']];
